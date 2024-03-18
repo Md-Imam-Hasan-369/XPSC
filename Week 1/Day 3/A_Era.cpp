500a3b9c247a2e613d8a89974ca36f079044df46
Md Imam Hasan <163656479+Md-Imam-Hasan-369@users.noreply.github.com>
2024-03-18 15:18:10 +0600

    Update A_Era.cpp

--- Week 1/Day 3/A_Era.cpp
+++ Week 1/Day 3/A_Era.cpp
@@ -27,3 +27,35 @@ int main()
     }
     return 0;
 }
+
+
+//SAME CODE JUST SINGLE CHANGE
+#include<bits/stdc++.h>
+using namespace std;
+
+//Speed
+#define Code ios_base::sync_with_stdio(false);
+#define By cin.tie(NULL);
+#define Imam cout.tie(NULL);
+
+int main()
+{
+    Code By Imam
+    int t;cin>>t;
+    while(t--)
+    {
+        int n;cin>>n;
+        vector<int> v(n+1);
+        for(int i=1;i<=n;i++) cin>>v[i];
+
+        int mx_val=0;//4
+        int op=0;//1
+        for(int i=1;i<=n;i++)
+        {
+            mx_val = max(mx_val,v[i]);
+            op = max(op,mx_val-i);//4-3=1
+        }
+        cout<<op<<"\n";
+    }
+    return 0;
+}
