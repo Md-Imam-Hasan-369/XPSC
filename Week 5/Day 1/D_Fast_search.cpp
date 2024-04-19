e0fa3cf2ecba6232bf83ccff2906a15a6affde27
Md Imam Hasan <163656479+Md-Imam-Hasan-369@users.noreply.github.com>
2024-04-20 00:08:21 +0600

    D_Fast_search.cpp

--- Week 5/Day 1/D_Fast_search.cpp
+++ Week 5/Day 1/D_Fast_search.cpp
@@ -6,29 +6,6 @@ using namespace std;
 #define By cin.tie(NULL);
 #define Imam cout.tie(NULL);
 
-int binary_search(int n,vector<int>& ar,int target)
-{
-    int idx=-1;
-    int l=0,r=n-1;
-    while(l<=r)
-    {
-        int mid = (l+r)/2;
-        if(ar[mid]== target)
-        {
-            idx = mid;
-            break;
-        }
-        else if(target < ar[mid])
-        {
-            r = mid-1;
-        }
-        else
-        {
-            l = mid+1;
-        }
-    }
-    return idx;
-}
 int main()
 {
     Code By Imam
@@ -43,30 +20,10 @@ int main()
     {
         int l,r;
         cin>>l>>r;
-        auto lb = lower_bound(ar.begin(),ar.end(),l);
-        auto rb = lower_bound(ar.begin(),ar.end(),r);
-
-        int l_idx = binary_search(n,ar,*lb);
-        int r_idx = binary_search(n,ar,*rb);
-
-        // cout<<*lb<<" "<<*rb<<endl;
-        // cout<<l_idx<<" "<<r_idx<<endl;
+        auto it_l = lower_bound(ar.begin(),ar.end(),l);
+        auto it_r = upper_bound(ar.begin(),ar.end(),r);
         
-        if(r_idx==-1)
-        {
-            if(l==*lb and r==*rb)
-            {
-                cout<<(r_idx-l_idx)+1<<endl;
-            }
-            else
-            {
-                cout<<(r_idx - l_idx)<<endl;
-            }
-        }
-        else
-        {
-            cout<<(n-l_idx)+1<<endl;
-        }
+        cout<<it_r - it_l<<" ";
     }
     return 0;
 }
