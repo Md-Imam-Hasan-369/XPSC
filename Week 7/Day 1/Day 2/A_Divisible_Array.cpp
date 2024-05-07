f5d2b3b45361f76d1a21c9551d99ca8c1cac1fb8
Md Imam Hasan <163656479+Md-Imam-Hasan-369@users.noreply.github.com>
2024-05-07 23:46:53 +0600

    Add files via upload
    Added week 7 day 2 codes

---
+++ Week 7/Day 1/Day 2/A_Divisible_Array.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a * b) / gcd(a, b))
+using namespace std;
+
+//Speed
+#define Code ios_base::sync_with_stdio(false);
+#define By cin.tie(NULL);
+#define Imam cout.tie(NULL);
+
+int main()
+{
+    Code By Imam
+    int t; cin>>t;
+    while(t--)
+    {
+        int n; cin>>n;
+        vector<int> ar(n);
+        for(int i=1;i<=n;i++)
+            ar[i-1] = i+i;
+        
+        for(int val:ar) cout<<val<<" ";
+        cout<<'\n';
+    }
+    return 0;
+}
