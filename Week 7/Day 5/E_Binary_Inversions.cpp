0658544c5806f622a45b933bfa6af97af6fd3390
Md Imam Hasan <163656479+Md-Imam-Hasan-369@users.noreply.github.com>
2024-05-11 01:20:36 +0600

    Update E_Binary_Inversions.cpp

--- Week 7/Day 5/E_Binary_Inversions.cpp
+++ Week 7/Day 5/E_Binary_Inversions.cpp
@@ -8,53 +8,69 @@ using namespace std;
 #define By cin.tie(NULL);
 #define Imam cout.tie(NULL);
 
-ll InvCnt(vector<int> a,int n){
-    ll res=0,one=0;
-    for(int i=0;i<n;i++){
-        if(a[i]==1){
-            one++;
-        }
-        else{
-            res+=one;
-        }
-    }
-    return res;
-}
-
 int main()
 {
     Code By Imam
-    int t; cin>>t;
+    ll t; cin>>t;
     while(t--)
     {
-        int n; cin>>n;
-        vector<int> a(n);
-        for(int i=0;i<n;i++){
+        ll n; cin>>n;
+        ll one=0;
+        vector<ll> a(n);
+        for(ll i=0;i<n;i++)
+        {
             cin>>a[i];
+            if(a[i]==1) one++;
         }
- 
-        ll ans=InvCnt(a,n);
-        int id=-1;
-        for(int i=0;i<n;i++){
-            if(a[i]==0){
-                a[i]=1;
-                id=i;
-                break;
-            }
-        }
-        ans=max(ans,InvCnt(a,n));
-        if(id!=-1){
-            a[id]=0;
-        }
-        for(int i=n-1;i>=0;i--){
-            if(a[i]==1){
-                a[i]=0;
-                break;
-            }
-        }
-        ans=max(ans,InvCnt(a,n));
-        cout<<ans<<endl;
 
+        vector<ll> b=a,c=a;
+        
+        //case 1
+        ll tmp1 = one;
+        ll ans1 = 0;
+        for(ll i=n-1;i>=0;i--)
+        {
+            if(a[i]==0 && tmp1>0)
+                ans1 += tmp1;
+            else tmp1--;
+        }
+        //case 2
+        for(ll i=0;i<n;i++)
+        {
+            if(b[i]==0)
+            {
+                b[i]=1;
+                break;
+            }
+        }
+        ll tmp2 = one+1;
+        ll ans2 = 0;
+        for(ll i=n-1;i>=0;i--)
+        {
+            if(b[i]==0 && tmp2>0)
+                ans2 += tmp2;
+            else tmp2--;
+        }
+        //case 3
+        for(ll i=n-1;i>=0;i--)
+        {
+            if(c[i]==1)
+            {
+                c[i]=0;
+                break;
+            }
+        }
+        ll tmp3 = one-1;
+        ll ans3 = 0;
+        for(ll i=n-1;i>=0;i--)
+        {
+            if(c[i]==0 && tmp3>0)
+                ans3 += tmp3;
+            else tmp3--;
+        }
+
+        ll ans = max({ans1,ans2,ans3});
+        cout<<ans<<'\n';
     }
     return 0;
 }
